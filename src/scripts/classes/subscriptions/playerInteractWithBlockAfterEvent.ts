70a5e3754d4ee061498918527a29558f02d85a45
Pete9xi <peter95rose@msn.com>
2025-04-02 16:11:45 +0100

    [WIP] Convert: chatSendAfterEvent, playerBreakBlockAfterEvent, playerBreakBlockBeforeEvent, playerInteractWithBlockAfterEvent, playerInteractWithBlockBeforeEvent over to TypeScript. Some functions are still yet to be converted.

---
+++ src/scripts/classes/subscriptions/playerInteractWithBlockAfterEvent.ts
@@ -0,0 +1,31 @@
+import { PlayerInteractWithBlockAfterEvent, world } from "@minecraft/server";
+import { SharedVariables } from "../../main";
+import { saveDoorPartsB } from "../../functions/saveDoorPartsB";
+
+// need a name for this
+function recordBlocks (event: PlayerInteractWithBlockAfterEvent){
+	if (SharedVariables.replayStateMachine.state === "recPending") {
+			const {
+				player,
+				block
+			} = event;
+			if (!SharedVariables.multiPlayers.includes(player)) return;
+			if (SharedVariables.twoPartBlocks.includes(block.type.id)) {
+				saveDoorPartsB(block, player);
+			} else {
+				const playerData = SharedVariables.replayBDataBMap.get(player.id);
+				playerData.dbgBlockDataB[SharedVariables.dbgRecTime] = {
+					location: block.location,
+					typeId: block.typeId,
+					states: block.permutation.getAllStates()
+				};
+			}
+		}
+} 
+
+
+const replaycraftInteractWithBlockAfterEvent = () => {
+	world.afterEvents.playerInteractWithBlock.subscribe(recordBlocks);
+};
+
+export { replaycraftInteractWithBlockAfterEvent }
---
+++ src/scripts/classes/subscriptions/playerInteractWithBlockBeforeEvent.ts
@@ -0,0 +1,30 @@
+import { PlayerInteractWithBlockBeforeEvent, world } from "@minecraft/server";
+import { SharedVariables } from "../../main";
+
+
+
+function recordBlocks (event: PlayerInteractWithBlockBeforeEvent){
+    if (SharedVariables.replayStateMachine.state === "recPending") {
+		const {
+			player,
+			block
+		} = event;
+		if (!SharedVariables.multiPlayers.includes(player)) return;
+		if (SharedVariables.twoPartBlocks.includes(block.type.id)) {
+			saveDoorParts1(block, player);
+		} else {
+			const playerData = SharedVariables.replayBData1Map.get(player.id);
+			playerData.dbgBlockData1[SharedVariables.dbgRecTime] = {
+				location: block.location,
+				typeId: block.typeId,
+				states: block.permutation.getAllStates()
+			};
+		}
+	}
+}
+
+
+const replaycraftInteractWithBlockBeforeEvent = () => {
+    world.beforeEvents.playerInteractWithBlock.subscribe(recordBlocks);
+};
+export { replaycraftInteractWithBlockBeforeEvent };
