f926864313cfae6c3f72118a580aee52512f6800
Pete9xi <peter95rose@msn.com>
2025-04-05 18:20:30 +0100

    [replayCraftDB] define the database component

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -13,6 +13,7 @@ import { BlockPermutation, EasingType, EquipmentSlot, system, world } from "@min
 import { clearStructure } from "./functions/clearStructure";
 import { playBlockSound } from "./functions/playBlockSound";
 import { onPlayerSpawn } from "./classes/subscriptions/player-spawn";
+import { OptimizedDatabase } from "./data/data-hive";
 //Global variables
 export let SharedVariables: SharedVariablesType = {
     soundIds: ['place.amethyst_block', 'place.amethyst_cluster', 'place.azalea', 'place.azalea_leaves', 'place.bamboo_wood', 'place.big_dripleaf', 'place.calcite', 'place.cherry_leaves', 'place.cherry_wood', 'place.chiseled_bookshelf', 'place.copper', 'place.copper_bulb', 'place.deepslate', 'place.deepslate_bricks', 'place.dirt_with_roots', 'place.dripstone_block', 'place.hanging_roots', 'place.large_amethyst_bud', 'place.medium_amethyst_bud', 'place.moss', 'place.nether_wood', 'place.pink_petals', 'place.pointed_dripstone', 'place.powder_snow', 'place.sculk', 'place.sculk_catalyst', 'place.sculk_sensor', 'place.sculk_shrieker', 'place.small_amethyst_bud', 'place.spore_blossom', 'place.tuff', 'place.tuff_bricks', "use.ancient_debris", "use.basalt", "use.bone_block", "use.candle", "use.cave_vines", "use.chain", "use.cloth", "use.copper", "use.coral", "use.deepslate", "use.deepslate_bricks", "use.dirt_with_roots", "use.dripstone_block", "use.grass", "use.gravel", "use.hanging_roots", "use.honey_block", "use.ladder", "use.moss", "use.nether_brick", "use.nether_gold_ore", "use.nether_sprouts", "use.nether_wart", "use.netherite", "use.netherrack", "use.nylium", "use.pointed_dripstone", "use.roots", "use.sand", "use.sculk_sensor", "use.shroomlight", "use.slime", "use.snow", "use.soul_sand", "use.soul_soil", "use.spore_blossom", "use.stem", "use.stone", "use.vines", "use.wood"],
@@ -80,6 +81,12 @@ replaycraftItemUseBeforeEvent();
 
 //Show the player a useful message for the first time they join!
 onPlayerSpawn();
+
+//data-hive
+const replayCraftDB = new OptimizedDatabase("replaceCraftDatabase");
+export{ replayCraftDB };
+
+
 
 //Timer for each frame?
 system.runInterval(() => {
