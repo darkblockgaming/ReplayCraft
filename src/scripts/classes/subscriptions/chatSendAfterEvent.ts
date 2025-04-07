e898cac973c551b31eecfa4b5cb0d675e0614d89
Pete9xi <peter95rose@msn.com>
2025-04-07 20:03:30 +0100

    remove testing commands

--- src/scripts/classes/subscriptions/chatSendAfterEvent.ts
+++ src/scripts/classes/subscriptions/chatSendAfterEvent.ts
@@ -1,7 +1,4 @@
 import { ChatSendAfterEvent, world } from "@minecraft/server";
-import { replayCraftBlockDB, replayCraftPlayerActionsDB, replayCraftPlayerPosDB, replayCraftPlayerRotDB } from "./world-initialize";
-import { SharedVariables } from "../../main";
-import { loadFromDB } from "../../functions/replayControls/load-from-database";
 
 function giveItems(event: ChatSendAfterEvent) {
 	const {sender, message} = event;
@@ -13,22 +10,7 @@ function giveItems(event: ChatSendAfterEvent) {
 			}]
 		});
 	}
-	if(message ==="?listdb"){
-		console.log(`data in map: `);
-		console.log(JSON.stringify(Object.fromEntries(SharedVariables.replayBDataMap), null, 2));
-		console.log("Data in the BlockDB: ");
-		console.log(JSON.stringify(replayCraftBlockDB.get(sender.id), null, 2));
-		console.log("Data in the PlayerPosDB: ");
-		console.log(JSON.stringify(replayCraftPlayerPosDB.get(sender.id), null, 2));
-		console.log("Data in the PlayerRotDB: ");
-		console.log(JSON.stringify(replayCraftPlayerRotDB.get(sender.id), null, 2));
-		console.log("Data in the PlayerActionsDB: ");
-		console.log(JSON.stringify(replayCraftPlayerActionsDB.get(sender.id), null, 2));
-        //sender.sendMessage(JSON.stringify(replayCraftDB.get(sender.id), null, 2));
-	}
-	if(message === "?loaddb"){
-	loadFromDB(sender, "test");
-	}
+	
 
 }
 
