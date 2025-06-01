4551878f39f8d778ae83bed06d2f3cf67ff6b263
Pete9xi <peter95rose@msn.com>
2025-06-01 17:01:51 +0100

    [replay-settings] Add Skin Settings button the the last UI menu allowing the player to configure skin settings from the ui as they could do before.

--- src/scripts/ui/replayCraft2F.ts
+++ src/scripts/ui/replayCraft2F.ts
@@ -1,4 +1,3 @@
-
 import * as ui from "@minecraft/server-ui";
 import { cancelRec } from "./cancelRec";
 import { replaySettings } from "./settings/replaySettings";
@@ -7,25 +6,29 @@ import { doReplay } from "../functions/replayControls/doReplay";
 import { doStopReplay } from "../functions/replayControls/doStopReplay";
 import { doCamSetupGoBack } from "../functions/camera/doCamSetupGoBack";
 import { loadBuildName } from "./load-buildname";
-export function ReplayCraft2F(player: Player) { //if SharedVariables.replayStateMachine.state = recSaved
+import { setSkin } from "./settings/setSkin";
+export function ReplayCraft2F(player: Player) {
+    //if SharedVariables.replayStateMachine.state = recSaved
     const replayForm = new ui.ActionFormData()
         .title("replaycraft.replaymenu.title")
         .button("dbg.rc1.button.start.replay") //0
         .button("dbg.rc1.button.stop.replay") //1
         .button("dbg.rc1.button.settings") //2
-        .button("dbg.rc1.button.goback.camsetup") //3
-        .button("dbg.rc1.button.load.structure.or.reset") //4
-        .button("dbg.rc1.button.load.session") //5
+        .button("replaycarft.setskin.button") //3
+        .button("dbg.rc1.button.goback.camsetup") //4
+        .button("dbg.rc1.button.load.structure.or.reset") //5
+        .button("dbg.rc1.button.load.session") //6
         .body("replaycraft.finalize.record.body");
-    replayForm.show(player).then(result => {
+    replayForm.show(player).then((result) => {
         if (result.canceled) return;
         const actions = {
             0: () => doReplay(player),
             1: () => doStopReplay(player),
             2: () => replaySettings(player),
-            3: () => doCamSetupGoBack(player),
-            4: () => cancelRec(player),
-            5: () => loadBuildName(player),
+            3: () => setSkin(player),
+            4: () => doCamSetupGoBack(player),
+            5: () => cancelRec(player),
+            6: () => loadBuildName(player),
         };
         const selectedAction = actions[result.selection as keyof typeof actions];
         if (selectedAction) {
--- src/scripts/ui/settings/replaySettings.ts
+++ src/scripts/ui/settings/replaySettings.ts
@@ -1,4 +1,3 @@
-
 import * as ui from "@minecraft/server-ui";
 import { SharedVariables } from "../../main";
 import { Player } from "@minecraft/server";
@@ -7,9 +6,11 @@ export function replaySettings(player: Player) {
     if (SharedVariables.currentSwitch === true) {
         if (SharedVariables.textPrompt) {
             player.sendMessage({
-                "rawtext": [{
-                    "translate": "dbg.rc1.mes.please.wait.for.replay.to.be.completed"
-                }]
+                rawtext: [
+                    {
+                        translate: "dbg.rc1.mes.please.wait.for.replay.to.be.completed",
+                    },
+                ],
             });
         }
         if (SharedVariables.soundCue) {
@@ -20,24 +21,24 @@ export function replaySettings(player: Player) {
     const playerName = SharedVariables.multiPlayers.map((player: Player) => player.name);
     const replaySettingsForm = new ui.ModalFormData()
         .title("dbg.rc1.title.replay.settings")
-        .dropdown("dbg.rc1.dropdown.title.replay.type", ["Default Replay", "Ghost Replay"], {defaultValueIndex: SharedVariables.settReplayType})
-        //.dropdown("dbg.rc1.dropdown.title.replay.skin.type", SharedVariables.skinTypes, {defaultValueIndex: SharedVariables.choosenReplaySkin})
-        .dropdown("dbg.rc1.dropdown.title.name.of.replay.player", ["Disable", "Player's Name", "Custom Name"], {defaultValueIndex: SharedVariables.settNameType})
+        .dropdown("dbg.rc1.dropdown.title.replay.type", ["Default Replay", "Ghost Replay"], { defaultValueIndex: SharedVariables.settReplayType })
+        .dropdown("dbg.rc1.dropdown.title.name.of.replay.player", ["Disable", "Player's Name", "Custom Name"], { defaultValueIndex: SharedVariables.settNameType })
         .textField("dbg.rc1.textfield.custom.name", SharedVariables.settCustomName)
-        .dropdown("dbg.rc1.dropdown.title.camera.ease.type", SharedVariables.easeTypes, {defaultValueIndex: SharedVariables.replayCamEase})
-        .dropdown("dbg.rc1.dropdown.title.camera.type", ["None (Free Cam)", "Cinematic Cam", "Focus Cam", "Top-Down Focus (Fixed)", "Top-Down Focus (Dynamic)"], {defaultValueIndex: SharedVariables.settCameraType})
-        .dropdown("dbg.rc1.dropdown.title.focus.on.player", playerName, {defaultValueIndex: SharedVariables.focusPlayerSelection})
-        .dropdown("dbg.rc1.dropdown.title.affect.camera.of.players", ["All Players", ...playerName], {defaultValueIndex: SharedVariables.affectCameraSelection})
-        .slider("drop.title.topdown.cam.height", 2, 20, {valueStep: 1,defaultValue: SharedVariables.topDownCamHight});
+        .dropdown("dbg.rc1.dropdown.title.camera.ease.type", SharedVariables.easeTypes, { defaultValueIndex: SharedVariables.replayCamEase })
+        .dropdown("dbg.rc1.dropdown.title.camera.type", ["None (Free Cam)", "Cinematic Cam", "Focus Cam", "Top-Down Focus (Fixed)", "Top-Down Focus (Dynamic)"], { defaultValueIndex: SharedVariables.settCameraType })
+        .dropdown("dbg.rc1.dropdown.title.focus.on.player", playerName, { defaultValueIndex: SharedVariables.focusPlayerSelection })
+        .dropdown("dbg.rc1.dropdown.title.affect.camera.of.players", ["All Players", ...playerName], { defaultValueIndex: SharedVariables.affectCameraSelection })
+        .slider("drop.title.topdown.cam.height", 2, 20, { valueStep: 1, defaultValue: SharedVariables.topDownCamHight });
 
-
-    replaySettingsForm.show(player).then(response => {
+    replaySettingsForm.show(player).then((response) => {
         if (response.canceled) {
             if (SharedVariables.textPrompt) {
                 player.sendMessage({
-                    "rawtext": [{
-                        "translate": "dbg.rc1.mes.please.click.submit"
-                    }]
+                    rawtext: [
+                        {
+                            translate: "dbg.rc1.mes.please.click.submit",
+                        },
+                    ],
                 });
             }
             if (SharedVariables.soundCue) {
@@ -46,8 +47,7 @@ export function replaySettings(player: Player) {
             return;
         }
         SharedVariables.settReplayType = Number(response.formValues[0]);
-        //SharedVariables.choosenReplaySkin = Number (response.formValues[1]);
-        SharedVariables.settNameType = Number(response.formValues[1]);;
+        SharedVariables.settNameType = Number(response.formValues[1]);
         SharedVariables.settCustomName = String(response.formValues[2]);
         SharedVariables.replayCamEase = Number(response.formValues[3]);
         SharedVariables.settCameraType = Number(response.formValues[4]);
@@ -61,5 +61,5 @@ export function replaySettings(player: Player) {
             SharedVariables.dbgCamAffectPlayer[0] = SharedVariables.multiPlayers[SharedVariables.affectCameraSelection - 1];
         }
         SharedVariables.topDownCamHight = Number(response.formValues[7]);
-    })
+    });
 }
