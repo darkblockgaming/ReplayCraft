aa2926d7d60b258eed7051d5b3caa7873bd5af08
Pete9xi <peter95rose@msn.com>
2025-07-20 20:12:52 +0100

    [skin] custom slash command to open the set skin ui

--- src/ReplayCraft BP/scripts/commands/command-handler.ts
+++ src/ReplayCraft BP/scripts/commands/command-handler.ts
@@ -1,9 +1,15 @@
 import { system, CustomCommand, CommandPermissionLevel, CustomCommandParamType, StartupEvent, CustomCommandResult, CustomCommandStatus, CustomCommandOrigin, Player } from "@minecraft/server";
 import { givePlayerControls } from "./player commands/control-items";
+import { playerSetSkin } from "./player commands/skin";
 function init(event: StartupEvent) {
     const replaycraftControlsCommand: CustomCommand = {
         name: "rc:controls",
         description: "gives you the replaycraft controls items.",
+        permissionLevel: CommandPermissionLevel.Any,
+    };
+    const replaycraftSkingCommand: CustomCommand = {
+        name: "rc:skin",
+        description: "Allows you to set your skin to be used in a replay.(Opens Skin UI)",
         permissionLevel: CommandPermissionLevel.Any,
     };
 
@@ -11,6 +17,7 @@ function init(event: StartupEvent) {
      * Register commands
      **/
     event.customCommandRegistry.registerCommand(replaycraftControlsCommand, givePlayerControls);
+    event.customCommandRegistry.registerCommand(replaycraftSkingCommand, playerSetSkin);
 }
 
 // Subscribe to startup event to register commands
