17a3c58744b3985c8a01e2d8ffdf0249b06f4d50
Pete9xi <peter95rose@msn.com>
2025-08-29 15:04:55 +0100

    rotate the trident as its facing backwards when spawned

--- src/ReplayCraft BP/scripts/main.ts
+++ src/ReplayCraft BP/scripts/main.ts
@@ -570,6 +570,7 @@ system.runInterval(() => {
 
                                     const projComp = spawnedEntity.getComponent("minecraft:projectile");
                                     if (projComp && data.velocity) {
+                                        spawnedEntity.teleport(tickData.location, { rotation: tickData.rotation });
                                         projComp.shoot(data.velocity);
                                     }
 
