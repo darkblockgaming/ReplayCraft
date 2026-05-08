7767496f1d9788972cd5a41e3d2c3c349754c5a6
Pete9xi <peter95rose@msn.com>
2026-05-08 22:06:50 +0100

    Refactor pause and resume command to check if the session exists and if so further check the state machine to make sure a session is in the correct state. added translation key to error messages

--- src/ReplayCraft BP/scripts/replay/commands/player commands/resume-recording-cmd.ts
+++ src/ReplayCraft BP/scripts/replay/commands/player commands/resume-recording-cmd.ts
@@ -1,11 +1,19 @@
 import { CustomCommandOrigin, CustomCommandStatus, Player, system } from "@minecraft/server";
 import { doResume } from "../../functions/replayControls/pause-replay-recording";
+import { replaySessions } from "../../data/replay-player-session";
 
 export function playerResumeRecordingCmd(_origin: CustomCommandOrigin) {
     const entity = _origin.sourceEntity;
     const sender = entity as Player;
+    const session = replaySessions.playerSessions.get(sender.id);
     system.run(() => {
-        doResume(sender);
+        if (!session) {
+            sender.sendMessage({
+                rawtext: [{ translate: "rc1.session.error.message" }],
+            });
+            return;
+        }
+        if (session.replayStateMachine.state == "recPaused") doResume(sender);
     });
 
     return {
