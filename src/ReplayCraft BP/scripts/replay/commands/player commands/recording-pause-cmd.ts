c42445d2fbf490e4224e2315a8e6e46965a1a7c4
Pete9xi <peter95rose@msn.com>
2026-06-08 18:16:13 +0100

    [playerPauseRecordingCmd] We check to make sure there is a current session and if so we further check to make sure the recording is active if not we return.

--- src/ReplayCraft BP/scripts/replay/commands/player commands/recording-pause-cmd.ts
+++ src/ReplayCraft BP/scripts/replay/commands/player commands/recording-pause-cmd.ts
@@ -1,10 +1,20 @@
 import { CustomCommandOrigin, CustomCommandStatus, Player, system } from "@minecraft/server";
 import { doPause } from "../../functions/replayControls/pause-replay-playback";
+import { replaySessions } from "../../data/replay-player-session";
 
 export function playerPauseRecordingCmd(_origin: CustomCommandOrigin) {
     const entity = _origin.sourceEntity;
     const sender = entity as Player;
+    const session = replaySessions.playerSessions.get(sender.id);
     system.run(() => {
+        if (!session) {
+            sender.sendMessage("§c[ReplayCraft] Error: No replay session found for you.");
+            return;
+        }
+        if (!session.replayStateMachine.states.recPending) {
+            sender.sendMessage("§c[ReplayCraft] Error: You are not currently recording.");
+            return;
+        }
         doPause(sender);
     });
 
